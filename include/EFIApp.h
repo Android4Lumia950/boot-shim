aab39e6864f42b49bb5d0dbc30985f549447dcfb
djokcho <140524342+djokcho@users.noreply.github.com>
2024-07-31 17:09:38 +0300

    Update EFIApp.h

--- include/EFIApp.h
+++ include/EFIApp.h
@@ -1,5 +1,4 @@
 #pragma once
-#ifndef _EFIAPP_H_
 
 #include <efi.h>
 #include <efilib.h>
@@ -8,33 +7,31 @@
 #include "elf.h"
 #include "ProcessorSupport.h"
 
-// Util for Stall
-#ifndef _EFI_STALL_UTIL_
-#define _EFI_STALL_UTIL_
-#define SECONDS_TO_MICROSECONDS(x) x * 1000000
-#endif
+// Utility macros
+#define SECONDS_TO_MICROSECONDS(x) ((x) * 1000000)
 
-/* low level macros for accessing memory mapped hardware registers */
+// Low-level macros for accessing memory-mapped hardware registers
 #define REG64(addr) ((volatile uint64_t *)(addr))
 #define REG32(addr) ((volatile uint32_t *)(addr))
 #define REG16(addr) ((volatile uint16_t *)(addr))
 #define REG8(addr) ((volatile uint8_t *)(addr))
 
-#define writel(v, a) (*REG32(a) = (v))
-#define readl(a) (*REG32(a))
+#define writel(v, a) (*(REG32(a)) = (v))
+#define readl(a) (*(REG32(a)))
 
-#define APPS_SS_BASE                0x0B000000
+// Define hardware register base addresses
+#define APPS_SS_BASE 0x0B000000
 
-#define MSM_GIC_DIST_BASE           APPS_SS_BASE
-#define GIC_DIST_REG(off)           (MSM_GIC_DIST_BASE + (off))
+#define MSM_GIC_DIST_BASE APPS_SS_BASE
+#define GIC_DIST_REG(off) (MSM_GIC_DIST_BASE + (off))
 
-#define GIC_DIST_CTRL               GIC_DIST_REG(0x000)
+#define GIC_DIST_CTRL GIC_DIST_REG(0x000)
 
-BOOLEAN CheckElf32Header(Elf32_Ehdr* header);
+// Function prototypes
+BOOLEAN CheckElf32Header(const Elf32_Ehdr* header);
 VOID JumpToAddress(
-	EFI_HANDLE ImageHandle,
-	EFI_PHYSICAL_ADDRESS Address,
-	VOID* PayloadBuffer,
-	UINTN PayloadLength
+    EFI_HANDLE ImageHandle,
+    EFI_PHYSICAL_ADDRESS Address,
+    VOID* PayloadBuffer,
+    UINTN PayloadLength
 );
-#endif
