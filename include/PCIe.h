3b10242a349d9570958e76cd3907365566699cce
djokcho <140524342+djokcho@users.noreply.github.com>
2024-07-31 17:33:24 +0300

    Update PCIe.h

--- include/PCIe.h
+++ include/PCIe.h
@@ -1,9 +1,10 @@
 #pragma once
 
 /**
-@file  EFIPCIeInit.h
-@brief Charger UEFI Protocol definitions.
-*/
+ * @file  EFIPCIeInit.h
+ * @brief Charger UEFI Protocol definitions.
+ */
+
 /*=============================================================================
 Copyright (c) 2011-2014 Qualcomm Technologies, Incorporated.
 All rights reserved.
@@ -13,53 +14,62 @@ Qualcomm Technologies, Confidential and Proprietary.
 /*=============================================================================
 EDIT HISTORY
 
-
 when       who     what, where, why
 --------   ---     -----------------------------------------------------------
 12/03/14   tselvam Added support for De-initialization
 06/16/14   hk      created
 
 =============================================================================*/
+
 #ifndef __EFIPCIEINIT_H__
 #define __EFIPCIEINIT_H__
 
+#include <efi.h>
+#include <efilib.h>
+
+/*=============================================================================
+GUID and Protocol Definitions
+=============================================================================*/
 #define EFI_QCOM_PCIE_INIT_GUID \
     { 0x93B80004, 0x9FB3, 0x11d4, { 0x9A, 0x3A, 0x00, 0x90, 0x27, 0x3F, 0xC1, 0x4D } }
-#define QCOM_PCIE_INIT_PROTOCOL EFI_QCOM_PCIE_INIT_GUID
 
-EFI_GUID gQcomPcieInitProtocolGuid = EFI_QCOM_PCIE_INIT_GUID;
+extern EFI_GUID gQcomPcieInitProtocolGuid;
 
 typedef struct _QCOM_PCIE_PROTOCOL QCOM_PCIE_PROTOCOL;
 
-/*===========================================================================
-FUNCTION DEFINITIONS
-===========================================================================*/
+/*=============================================================================
+Function Declarations
+=============================================================================*/
 typedef
-EFI_STATUS(EFIAPI * EFI_PCIE_INIT)(IN QCOM_PCIE_PROTOCOL *This);
+EFI_STATUS (EFIAPI * EFI_PCIE_INIT)(IN QCOM_PCIE_PROTOCOL *This);
+
 typedef
-UINT32(EFIAPI * EFI_PCI_RP_READ_CONFIG_32)(IN UINT32 rpIndex, IN UINT32 offset);
+UINT32 (EFIAPI * EFI_PCI_RP_READ_CONFIG_32)(IN UINT32 rpIndex, IN UINT32 offset);
+
 typedef
-EFI_STATUS(EFIAPI * EFI_PCIE_DEINIT)(void);
+EFI_STATUS (EFIAPI * EFI_PCIE_DEINIT)(VOID);
 
-
-/*===========================================================================
-PROTOCOL INTERFACE
-===========================================================================*/
-
+/*=============================================================================
+Protocol Interface
+=============================================================================*/
 struct _QCOM_PCIE_PROTOCOL {
-	EFI_PCIE_INIT   PCIeInitHardware;
-	EFI_PCI_RP_READ_CONFIG_32 PCIe_Config_Read_32;
-	EFI_PCIE_DEINIT PCIeDeInitHardware;
+    EFI_PCIE_INIT           PCIeInitHardware;
+    EFI_PCI_RP_READ_CONFIG_32 PCIeConfigRead32;
+    EFI_PCIE_DEINIT         PCIeDeInitHardware;
 };
 
+/*=============================================================================
+Hardware Register Access Macros
+=============================================================================*/
 #define REG32(addr) ((volatile uint32_t *)(addr))
-#define readl(a) (*REG32(a))
+#define readl(a)   (*REG32(a))
 
-#define PCIE_PHY_PCS_STATUS	0x728
-
-#define MSM_PCIE_PHY 0xfc52e000
-
-#define REFCLK_STABILIZATION_DELAY_US_MIN     1000
-#define PHY_READY_TIMEOUT_COUNT		   10
+/*=============================================================================
+Constants
+=============================================================================*/
+#define PCIE_PHY_PCS_STATUS             0x728
+#define MSM_PCIE_PHY                    0xfc52e000
+#define REFCLK_STABILIZATION_DELAY_US_MIN 1000
+#define PHY_READY_TIMEOUT_COUNT         10
 
 #endif  /* __EFIPCIEINIT_H__ */
